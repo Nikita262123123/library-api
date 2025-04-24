43a62c94e7af2a32fe931338dc3d0705705af565
Nikita <nikitalabut262@gmail.com>
2025-04-24 15:19:58 +0300

    fix exception messages

--- book-service/src/main/java/by/modsen/bookservice/service/BookService.java
+++ book-service/src/main/java/by/modsen/bookservice/service/BookService.java
@@ -7,6 +7,7 @@ import by.modsen.bookservice.exception.BookNotFoundException;
 import by.modsen.bookservice.exception.BookWithIsbnExistException;
 import by.modsen.bookservice.mapper.BookMapper;
 import by.modsen.bookservice.repository.BookRepository;
+import by.modsen.bookservice.util.ExceptionConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +23,7 @@ public class BookService {
 
     public BookResponse getBookById(Long id) {
         Book book = bookRepository.findById(id)
-                .orElseThrow(() -> new BookNotFoundException("Книга с id = " + id + " не найдена"));
+                .orElseThrow(() -> new BookNotFoundException(String.format(ExceptionConstants.BOOK_NOT_FOUND, id)));
         return bookMapper.toBookResponse(book);
     }
 
@@ -33,14 +34,14 @@ public class BookService {
 
     public BookResponse getBookByIsbn(String isbn) {
         Book book = bookRepository.findByIsbn(isbn)
-                .orElseThrow(() -> new BookNotFoundException("Книга с ISBN = " + isbn + " не найдена"));
+                .orElseThrow(() -> new BookNotFoundException(String.format(ExceptionConstants.BOOK_WITH_ISBN_NOT_FOUND, isbn)));
         return bookMapper.toBookResponse(book);
     }
 
     @Transactional
     public BookResponse addBook(BookRequest bookRequest) {
         if (bookRepository.findByIsbn(bookRequest.isbn()).isPresent()) {
-            throw new BookWithIsbnExistException("Книга с этим ISBN уже существует");
+            throw new BookWithIsbnExistException(ExceptionConstants.BOOK_WITH_ISBN_EXIST);
         }
         Book book = bookMapper.toBookEntity(bookRequest);
         bookRepository.save(book);
@@ -50,9 +51,9 @@ public class BookService {
     @Transactional
     public BookResponse updateBook(Long id, BookRequest bookRequest) {
         Book existingBook = bookRepository.findById(id)
-                .orElseThrow(() -> new BookNotFoundException("Книга с id = " + id + " не найдена"));
+                .orElseThrow(() -> new BookNotFoundException(String.format(ExceptionConstants.BOOK_NOT_FOUND, id)));
         if (!existingBook.getIsbn().equals(bookRequest.isbn()) && bookRepository.existsByIsbn(bookRequest.isbn())) {
-            throw new BookWithIsbnExistException("Книга с этим ISBN уже существует");
+            throw new BookWithIsbnExistException(ExceptionConstants.BOOK_WITH_ISBN_EXIST);
         }
         bookMapper.updateBookFromDTO(bookRequest, existingBook);
         bookRepository.save(existingBook);
@@ -62,7 +63,7 @@ public class BookService {
     @Transactional
     public void deleteBook(Long id) {
         if (!bookRepository.existsById(id)) {
-            throw new BookNotFoundException("Книга с id = " + id + " не найдена");
+            throw new BookNotFoundException(String.format(ExceptionConstants.BOOK_NOT_FOUND, id));
         }
         bookRepository.deleteById(id);
     }
---
+++ book-service/src/main/java/by/modsen/bookservice/util/ExceptionConstants.java
@@ -0,0 +1,13 @@
+package by.modsen.bookservice.util;
+
+public final class ExceptionConstants {
+
+    public static final String BOOK_NOT_FOUND = "Книга с ID = %d не найдена";
+    public static final String BOOK_WITH_ISBN_NOT_FOUND = "Книга с ISBN = %s не найдена";
+    public static final String BOOK_WITH_ISBN_EXIST = "Книга с этим ISBN уже существует";
+
+
+
+
+    private ExceptionConstants() {}
+}
